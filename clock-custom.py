52f4de3214c458f6c25eb8c73efbcca1b48c5e5a
Evan Gallis <evanrgallis@gmail.com>
2024-02-25 16:14:50 -0800

    Create clock-custom.py

---
+++ clock-custom.py
@@ -0,0 +1,38 @@
+from pwnagotchi.ui.components import LabeledValue
+from pwnagotchi.ui.view import BLACK
+import pwnagotchi.ui.fonts as fonts
+import pwnagotchi.plugins as plugins
+import pwnagotchi
+import logging
+import datetime
+
+class PwnClock(plugins.Plugin):
+    __author__ = 'originally https://github.com/LoganMD redone by NeonLightning'
+    __version__ = '1.0.3'
+    __license__ = 'GPL3'
+    __description__ = 'Clock/Calendar for pwnagotchi'
+
+    def on_loaded(self):
+        logging.info("Pwnagotchi Clock Plugin loaded.")
+
+    def on_ui_setup(self, ui):
+        pos1 = (200, 95)
+        ui.add_element('clock1', LabeledValue(color=BLACK, label='', value='-/-/-',
+                                                position=pos1,
+                                                label_font=fonts.Small, text_font=fonts.Small))
+        pos2 = (150, 95)
+        ui.add_element('clock2', LabeledValue(color=BLACK, label='', value='-:--',
+                                                position=pos2,
+                                                label_font=fonts.Small, text_font=fonts.Small))
+        
+    def on_ui_update(self, ui):
+        now = datetime.datetime.now()
+        datenow = now.strftime("%m/%d/%y")
+        timenow = now.strftime("%I:%M%p")
+        ui.set('clock1', datenow)
+        ui.set('clock2', timenow)
+
+    def on_unload(self, ui):
+        with ui._lock:
+            ui.remove_element('clock1')
+            ui.remove_element('clock2')
